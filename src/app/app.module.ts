9cc5f53dfbaab9ba0c7875cccc70c4103229db42
auliaisalsabila <aulia1800016089@webmail.uad.ac.id>
2020-04-01 18:56:04 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentComponent } from './pages/student/student.component';
+import { Praktikum3Component } from './pages/praktikum3/praktikum3.component';
+import { Praktikum4Component } from './pages/praktikum4/praktikum4.component';
+import { Praktikum5Component } from './pages/praktikum5/praktikum5.component';
+import { Praktikum6Component } from './pages/praktikum6/praktikum6.component';
+import { Praktikum7Component } from './pages/praktikum7/praktikum7.component';
+import { Praktikum8Component } from './pages/praktikum8/praktikum8.component';
+import { Praktikum9Component } from './pages/praktikum9/praktikum9.component';
+import { Praktikum10Component } from './pages/praktikum10/praktikum10.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudentComponent,
+    Praktikum3Component,
+    Praktikum4Component,
+    Praktikum5Component,
+    Praktikum6Component,
+    Praktikum7Component,
+    Praktikum8Component,
+    Praktikum9Component,
+    Praktikum10Component
   ],
   imports: [
     BrowserModule,
